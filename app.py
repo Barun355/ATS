2ffb06cca48b75a847e20c5d77382c4ff05a3026
Barun355 <baruntiwary620@gmail.com>
2024-02-06 16:05:40 +0530

    Add donation section

--- app.py
+++ app.py
@@ -44,49 +44,59 @@ def input_pdf_setup(upload_file):
 ## Streamlit App
 
 st.set_page_config(page_title="RF-Automation: ATS Resume Expert")
-st.header("Application Tracking System");
 
-input_text = st.text_area("Job Description: ", key="input")
-uploaded_file = st.file_uploader("Upload Your Resume(PDF)", type=["pdf"])
+def ats():
+    st.header("Application Tracking System");
 
-
-if uploaded_file is not None:
-    st.write("PDF Uploaded Successfully")
+    input_text = st.text_area("Job Description: ", key="input")
+    uploaded_file = st.file_uploader("Upload Your Resume(PDF)", type=["pdf"])
 
 
-submit1 = st.button("Tell Me About the Resume")
-# submit2 = st.button("How can I improve my skills")
-submit3 = st.button("Percentage Match")
-
-
-input_prompt1 = """
-    You are an experienced Technical HR Manager with Tech Experience in the field of any job role from Data Science, Full Stack Web Devlopment, Big Data Engineering, DEVOPS, Data Analyst, your task is to review
-    the provided resume against the job description for this profiles.
-    Please share your professional evaluation on whether the candidate's profile aligns with the role.
-    Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
-"""
-
-input_prompt3 = """
-    You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of Data Science, Full Stack Web Devlopment, Big Data Engineering, DEVOPS, Data Analyst, and deep ATS functionality, 
-    your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
-    the job description. First the output should come as percentage and then keywords missing and last final thoughts.
-"""
-
-
-if submit1:
     if uploaded_file is not None:
-        pdf_content = input_pdf_setup(uploaded_file)
-        response = get_gemini_response(input_prompt1, pdf_content, input_text)
-        st.subheader("The Response is: ")
-        st.write(response)
-    else:
-        st.write("Please Upload a pdf")
+        st.write("PDF Uploaded Successfully")
 
-elif submit3:
-    if uploaded_file is not None:
-        pdf_content = input_pdf_setup(uploaded_file)
-        response = get_gemini_response(input_prompt3, pdf_content, input_text)
-        st.subheader("The Response is: ")
-        st.write(response)
-    else:
-        st.write("Please Upload a pdf")
+    submit1 = st.button("Tell Me About the Resume")
+    # submit2 = st.button("How can I improve my skills")
+    submit3 = st.button("Percentage Match")
+
+
+    input_prompt1 = """
+        You are an experienced Technical HR Manager with Tech Experience in the field of any job role from Data Science, Full Stack Web Devlopment, Big Data Engineering, DEVOPS, Data Analyst, your task is to review
+        the provided resume against the job description for this profiles.
+        Please share your professional evaluation on whether the candidate's profile aligns with the role.
+        Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+    """
+
+    input_prompt3 = """
+        You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of Data Science, Full Stack Web Devlopment, Big Data Engineering, DEVOPS, Data Analyst, and deep ATS functionality, 
+        your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
+        the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+    """
+
+
+    if submit1:
+        if uploaded_file is not None:
+            pdf_content = input_pdf_setup(uploaded_file)
+            response = get_gemini_response(input_prompt1, pdf_content, input_text)
+            st.subheader("The Response is: ")
+            st.write(response)
+        else:
+            st.write("Please Upload a pdf")
+
+
+    elif submit3:
+        if uploaded_file is not None:
+            pdf_content = input_pdf_setup(uploaded_file)
+            response = get_gemini_response(input_prompt3, pdf_content, input_text)
+            st.subheader("The Response is: ")
+            st.write(response)
+        else:
+            st.write("Please Upload a pdf")
+
+def main():
+    
+    st.header("Help us to improve our service by donation")
+    st.image("QR.jpeg", width=200)
+    ats()
+    
+main()
